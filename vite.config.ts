3f55cd91abee40917bf2fdc41b36934551cc3da7
GuariVT <141346424+GuariVT@users.noreply.github.com>
2025-10-29 01:59:19 -0500

    Update base URL for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // ¡AÑADE ESTA LÍNEA!
+  base: '/franciscoArizagaluque.github.io/', // ¡URL ABSOLUTA de tu repo!
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
