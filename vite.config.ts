b0f2d71945fbfaaeb40d4e9d11ff92e025606b3d
GuariVT <141346424+GuariVT@users.noreply.github.com>
2025-11-04 14:50:45 -0500

    Update base URL in vite.config.ts
    Changed the base URL for the Vite configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/franciscoArizagaluque.github.io/', // ¡URL ABSOLUTA de tu repo!
+  base: '/', // ¡URL ABSOLUTA de tu repo!
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
